a5368497f4f52109b88647e0ad05407ad459f5b1
Maaz Ekkiswala <maazekkiswala8758@gmail.com>
2026-05-01 19:05:28 +0530

    harkaat three harf zer

---
+++ src/audio/harkaat/zer.ts
@@ -0,0 +1,11 @@
+export const zerAudio = {
+  azina: require("../../assets/audio/harkaat/zer/3harf/azina.m4a"),
+  bakhila: require("../../assets/audio/harkaat/zer/3harf/bakhila.m4a"),
+  baladi: require("../../assets/audio/harkaat/zer/3harf/baladi.m4a"),
+  falaqi: require("../../assets/audio/harkaat/zer/3harf/falaqi.m4a"),
+  ibil: require("../../assets/audio/harkaat/zer/3harf/ibil.m4a"),
+  khashiya: require("../../assets/audio/harkaat/zer/3harf/khashiya.m4a"),
+  malik: require("../../assets/audio/harkaat/zer/3harf/malik.m4a"),
+  raziya: require("../../assets/audio/harkaat/zer/3harf/raziya.m4a"),
+  waliya: require("../../assets/audio/harkaat/zer/3harf/alaa.m4a"),
+} as const;
