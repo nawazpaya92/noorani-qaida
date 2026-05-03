4c283c3da5dfe21b9ebac5347820c9eb4c81f91f
Maaz Ekkiswala <maazekkiswala8758@gmail.com>
2026-05-03 23:21:04 +0530

    yaa maddah 3 harf last

--- src/audio/hurufeMaddah/yaaMaddah.ts
+++ src/audio/hurufeMaddah/yaaMaddah.ts
@@ -12,4 +12,20 @@ export const yaaMaddahAudio = {
   yaa_madda_ma_sakin_tadwleelin: require("../../assets/audio/hurufeMaddah/yaaMaddah/maaSaakin/yaa_madda_ma_sakin_tadwleelin.m4a"),
   yaa_madda_ma_sakin_miskeeni: require("../../assets/audio/hurufeMaddah/yaaMaddah/maaSaakin/yaa_madda_ma_sakin_miskeeni.m4a"),
   yaa_madda_ma_sakin_taqweemin: require("../../assets/audio/hurufeMaddah/yaaMaddah/maaSaakin/yaa_madda_ma_sakin_taqweemin.m4a"),
+
+  //3harf
+  //last
+  yaa_madda_end_lafee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_lafee.m4a"),
+  yaa_madda_end_abee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_abee.m4a"),
+  yaa_madda_end_akhee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_akhee.m4a"),
+  yaa_madda_end_banee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_banee.m4a"),
+  yaa_madda_end_lazee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_lazee.m4a"),
+  yaa_madda_end_lizee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_lizee.m4a"),
+  yaa_madda_end_kulee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_kulee.m4a"),
+  yaa_madda_end_nuree: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_nuree.m4a"),
+  yaa_madda_end_latee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_latee.m4a"),
+  yaa_madda_end_afee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_afee.m4a"),
+  yaa_madda_end_wafee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_wafee.m4a"),
+  yaa_madda_end_fafee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_fafee.m4a"),
+  yaa_madda_end_taqee: require("../../assets/audio/hurufeMaddah/yaaMaddah/3harf/last/yaa_madda_end_taqee.m4a"),
 } as const;
